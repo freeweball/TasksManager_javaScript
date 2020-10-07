5180f724612d6fd7fc4fe89b4323f3ca18273639
Савостин Александр <asavostin1@gmail.com>
2020-10-07 15:14:38 +0500

    6.1.13

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -135,6 +135,7 @@ export default class TaskEdit extends AbstractView {
     this._data = TaskEdit.parseTaskToData(task);
 
     this._formSubmitHandler = this._formSubmitHandler.bind(this);
+    this._descriptionInputHandler = this._descriptionInputHandler.bind(this);
     this._dueDateToggleHandler = this._dueDateToggleHandler.bind(this);
     this._repeatingToggleHandler = this._repeatingToggleHandler.bind(this);
 
@@ -145,7 +146,7 @@ export default class TaskEdit extends AbstractView {
     return createTaskEditTemplate(this._data);
   }
 
-  updateData(update) {
+  updateData(update, justDataUpdating) {
     if (!update) {
       return;
     }
@@ -155,6 +156,10 @@ export default class TaskEdit extends AbstractView {
       this._data,
       update
     );
+
+    if (justDataUpdating) {
+      return;
+    }
 
     this.updateElement();
   }
@@ -184,6 +189,9 @@ export default class TaskEdit extends AbstractView {
     this.getElement()
       .querySelector(`.card__repeat-toggle`)
       .addEventListener(`click`, this._repeatingToggleHandler);
+    this.getElement()
+      .querySelector(`.card__text`)
+      .addEventListener(`input`, this._descriptionInputHandler);
   }
 
   _dueDateToggleHandler(evt) {
@@ -198,6 +206,13 @@ export default class TaskEdit extends AbstractView {
     this.updateData({
       isRepeating: !this._data.isRepeating
     });
+  }
+
+  _descriptionInputHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      description: evt.target.value
+    }, true);
   }
 
   _formSubmitHandler(evt) {
